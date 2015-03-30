ee31d66bcb543a37ec44e4ce18b86bfe08b341f5
Rommel Castro <rommelxcastro@gmail.com>
2015-03-30 19:56:17 -0400

    hello world with React

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+/** @jsx React.DOM */
+var hello = React.createClass({
+	render: function() {
+		return <div>hello {this.props.name}</div>
+	}
+});
 
+React.renderComponent(<hello name="world">, body);

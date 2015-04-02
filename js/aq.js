a50145e811c7c20328bc557ecb42c135533dc0fb
Rommel Castro <rommelxcastro@gmail.com>
2015-04-02 08:20:50 -0400

    formating changes, you know! webstorm rockz

--- js/aq.js
+++ js/aq.js
@@ -1,12 +1,28 @@
 /** @jsx React.DOM */
-(function() {
+(function () {
     'use strict';
 
     var Quiz = React.createClass({
-      render: function() {
-        return <div > test < /div>
-      }
+        propTypes: {
+            books: React.PropTypes.array.isRequired
+        },
+        render: function () {
+            return <div >
+                {this.props.books.map(function (b) {
+                    return <Book key={b} title={b}/>
+                })}
+            </div>
+        }
     });
 
-    React.render( <Quiz data={"foo"}/>, document.getElementById("app"));
-    })();
+    var Book = React.createClass({
+        propTypes: {
+            title: React.PropTypes.string.isRequired
+        },
+        render: function () {
+            return <div><h4>{this.props.title}</h4></div>
+        }
+    });
+
+    React.render(<Quiz books={['bang bang', 'rommel']}/>, document.getElementById("app"));
+})();

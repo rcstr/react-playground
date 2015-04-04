6828a66ff9a11790506664bce4e8e9790592a045
Rommel Castro <rommelxcastro@gmail.com>
2015-04-04 14:26:31 -0400

    authors/books data

--- js/aq.js
+++ js/aq.js
@@ -2,6 +2,26 @@
 (function () {
     'use strict';
 
+    var imgPath = '/img/authors/',
+        data = [{
+            name: 'Homer',
+            img: '159px-Homer_Musei_Capitolini_MC559.jpg',
+            books: ['Odyssey', 'Iliad']
+        }, {
+            name: 'Nicolás Maquiavelo',
+            img: '186px-Portrait_of_Niccolò_Machiavelli_by_Santi_di_Tito.jpg',
+            books: ['The Prince', 'Discourses on Livy', 'The Art of War', 'Florentine Histories', 'Life of Castruccio Castracani']
+        }, {
+            name: 'Charles Darwin',
+            img: '187px-Charles_Darwin_01.jpg',
+            books: ['The Voyage of the Beagle', 'The Autobiography of Charles Darwin', 'The Expression of the Emotions in Man and Animals', 'The Descent of Man, and Selection in Relation to Sex', 'On the origin of species by means of natural selection']
+        }, {
+            name: 'J.K Rowling',
+            img: 'J._K._Rowling_2010.jpg',
+            books: ['HArry Potter']
+        }];
+
+
     var Quiz = React.createClass({
         propTypes: {
             books: React.PropTypes.array.isRequired

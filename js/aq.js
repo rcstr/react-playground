d59bb05fcd2df29e7035c4efafe79ba6a3fc7f5c
Rommel Castro <rommelxcastro@gmail.com>
2015-04-05 09:22:39 -0600

    answer handle and some reformat

--- js/aq.js
+++ js/aq.js
@@ -24,18 +24,22 @@
 
     data.selectGame = function selectGame() {
         var books = _.shuffle(this.reduce(function (p, c, i) {
-            return p.concat(c.books);
-        }, [])).slice(0, 4);
-
-        var answer = books[_.random(books.length - 1)];
+                return p.concat(c.books);
+            }, [])).slice(0, 4),
+            answer = books[_.random(books.length - 1)];
 
         return {
             books: books,
-            author: _.find(this, function(author) {
-                return author.books.some(function(title) {
+            author: _.find(this, function (author) {
+                return author.books.some(function (title) {
                     return title === answer;
                 });
-            })
+            }),
+            checkAnswer: function (title) {
+                return this.author.books.some(function (t) {
+                    return t === title;
+                });
+            }
         };
     };
 
@@ -44,7 +48,22 @@
             data: React.PropTypes.array.isRequired
         },
         getInitialState: function () {
-            return this.props.data.selectGame();
+            return _.extend({
+                    bgClass: 'neutral',
+                    showContinue: false
+                },
+                this.props.data.selectGame());
+        },
+        handleBookSelected: function (title) {
+            var isCorrect = this.state.checkAnswer(title);
+
+            this.setState({
+                bgClass: isCorrect ? 'pass' : 'fail',
+                showContinue: isCorrect
+            });
+        },
+        handleContinue: function () {
+            this.setState(this.getInitialState);
         },
         render: function () {
             return (<div>
@@ -54,12 +73,18 @@
                     </div>
                     <div className="col-md-7">
                         {this.state.books.map(function (b) {
-                            return <Book title={b}/>
+                            return <Book onBookSelected={this.handleBookSelected} title={b}/>
                         }, this)}
                     </div>
-                    <div className="col-md-1"></div>
+                    <div className={'col-md-1 answer-state ' + this.state.bgClass}></div>
                 </div>
-            </div>)
+                {this.state.showContinue ? (<div className="row">
+                    <div className="col-md-12 text-right">
+                        <input type="button" className="btn btn-success" value="Continue" onClick={this.handleContinue}/>
+                    </div>
+                </div>) : (<span />) }
+            </div>
+            );
         }
     });
 
@@ -67,10 +92,13 @@
         propTypes: {
             title: React.PropTypes.string.isRequired
         },
+        handleClick: function () {
+            this.props.onBookSelected(this.props.title);
+        },
         render: function () {
-            return <div className="answer"><h4>{this.props.title}</h4></div>
+            return <div className="answer" onClick={this.handleClick}><h4>{this.props.title}</h4></div>
         }
     });
 
-    React.render(<Quiz data={data}/>, document.getElementById("app"));
+    React.render(<Quiz data={data}/>, document.getElementById('app'));
 })();
